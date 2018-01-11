e94f9436853604fb8ef46b2bc8008bda63205c15
刘志凌 <liuzhiling@liuzhilingdeMacBook-Pro.local>
2018-01-10 21:20:31 -0800

    add main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import  { Header } from './Header';
+import  { Main} from  './Main';
 
 
 class App extends Component {
@@ -9,9 +10,7 @@ class App extends Component {
 
       <div className="App">
           <Header/>
-        {/*<p className="App-intro">*/}
-          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
+        <Main/>
       </div>
     );
   }
