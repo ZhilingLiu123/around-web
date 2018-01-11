1cd3319a128645bf75e167522dec513fdc073737
刘志凌 <liuzhiling@liuzhilingdeMacBook-Pro.local>
2018-01-10 21:08:22 -0800

    style

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import  { Header } from './Header';
 class App extends Component {
   render() {
     return (
+
       <div className="App">
           <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*<p className="App-intro">*/}
+          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
       </div>
     );
   }
